260398680f0b5dec2ccd37c90a7415a1a61f09e5
Christopher Groskopf <chrisgroskopf@gmail.com>
2016-02-05 22:55:21 -0600

    Rev to 0.1.1 for dev.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ install_requires = [
 
 setup(
     name='agate-remote',
-    version='0.1.0',
+    version='0.1.1',
     description='agate-remote adds read support for remote files to agate.',
     long_description=open('README.rst').read(),
     author='Christopher Groskopf',
